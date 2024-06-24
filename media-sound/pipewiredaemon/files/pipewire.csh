117542bd8af85de2b84cce019da7cee7023580f7
ormorph <roma251078@mail.ru>
2024-06-24 21:43:59 +0300

    Binding to username, user ID is determined by name for pipewiredaemon

--- media-sound/pipewiredaemon/files/pipewire.csh
+++ media-sound/pipewiredaemon/files/pipewire.csh
@@ -3,7 +3,7 @@ if (( $user != "pipewire" )) then
 	if ( $? == 0 ) then
 		setenv PULSE_SERVER "unix:/tmp/pipewire-daemon-socket"
 		if ( ( ! -e "/run/user/$uid/pipewire-0" ) ) then
-			ln -sf /run/user/509/pipewire-0 "/run/user/$UID/pipewire-0"
+			ln -sf "/run/user/`id -u pipewire`/pipewire-0" "/run/user/$UID/pipewire-0"
 		endif
 	endif
 endif
--- media-sound/pipewiredaemon/files/pipewire.sh
+++ media-sound/pipewiredaemon/files/pipewire.sh
@@ -2,7 +2,7 @@ if [ "$USER" != "pipewire" ] ; then
 	if rc-status | grep -q pipewire ; then
 		export PULSE_SERVER="unix:/tmp/pipewire-daemon-socket"
 		if ! [[ -f "/run/user/$UID/pipewire-0" ]] ; then
-			ln -sf /run/user/509/pipewire-0 "/run/user/$UID/pipewire-0"
+			ln -sf "/run/user/$(id -u pipewire)/pipewire-0" "/run/user/$UID/pipewire-0"
 		fi
 	fi
 fi
